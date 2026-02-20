5d1e3208e38d3a89f58a7ca0f44efddb149abba9
bibek-2005 <bibekanandamuni@gmail.com>
2026-02-21 00:11:08 +0530

    Linked list concept

---
+++ src/main/java/LinkedList/DisplayList.java
@@ -0,0 +1,52 @@
+package LinkedList;
+
+import java.util.Scanner;
+
+
+
+public class DisplayList {
+    public static void Display(Node head){
+        Node temp = head;
+        while(temp!= null){
+            //System.out.print(temp.val + " ");
+            temp= temp.next;
+        }
+
+    }
+
+    public static void dsplayrec(Node head){
+        Node temp = head;
+        if(temp == null) return;
+        System.out.print(temp.val+" ");
+        dsplayrec(temp.next);
+
+    }
+    public static int get(Node head,int index){
+        Node temp=head;
+        for(int idx =0; idx<index; idx++){
+            temp = temp.next;
+        }
+        return temp.val;
+
+    }
+
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        Node a = new Node(12);
+        Node b = new Node(20);
+        Node c= new Node(30);
+        Node d= new Node(40);
+        Node e= new Node(50);
+        //System.out.println(c.next);
+        //connect the linked list
+        a.next = b;
+        b.next=c;
+        c.next= d;
+        d.next=e;
+        //e.next=a;
+        e.next=null;
+        //Display(a);
+        //dsplayrec(a);
+        System.out.println(get(a,3));
+    }
+}
---
+++ src/main/java/LinkedList/LinkedList.java
@@ -0,0 +1,15 @@
+package LinkedList;
+
+class Node{
+    int val;
+    Node next;
+    Node(int val){
+        this.val = val;
+    }
+}
+
+public class LinkedList {
+    public static void main(String[] args) {
+
+    }
+}
