a64c7661cbb2eeba09e20eb995c649d7381c1cfa
bibek-2005 <bibekanandamuni@gmail.com>
2026-03-15 23:51:37 +0530

    Binary tree

--- src/main/java/BinaryTree/Tree.java
+++ src/main/java/BinaryTree/Tree.java
@@ -1,5 +1,9 @@
 package BinaryTree;
 
+import java.sql.SQLOutput;
+import java.util.LinkedList;
+import java.util.Queue;
+
 class Node{
     int val;
     Node left;
@@ -8,7 +12,15 @@ class Node{
         this.val=val;
     }
 }
+class Pair{
+    int level;
+    Node node;
 
+    Pair(Node node,int level){
+        this.level=level;
+        this.node=node;
+    }
+}
 
 public class Tree {
     public static void main(String[] args) {
@@ -26,20 +38,47 @@ public class Tree {
         c.left=f;c.right=g;
 
         //System.out.print(a.val+" ");
-        display(a);
-        System.out.println();
-        System.out.print(size(a));
-        System.out.println();
-        System.out.print(sum(a));
-        System.out.println();
-        System.out.print(product(a));
-        System.out.println();
-        System.out.print(maxi(a));
-        System.out.println();
-        System.out.print(mini(a));
-
+        //display(a);
+//        System.out.println();
+//        System.out.print(size(a));
+//        System.out.println();
+//        System.out.print(sum(a));
+//        System.out.println();
+//        System.out.print(product(a));
+//        System.out.println();
+//        System.out.print(maxi(a));
+//        System.out.println();
+//        System.out.print(mini(a));
+//        System.out.println();
+//        System.out.print(level(a));
+//        preorder(a);
+//        System.out.println();
+//        postorder(a);
+//        System.out.println();
+//        inorder(a);
+        //levelOrder(a);
+        //System.out.println(levelOrder(a););
+        levelrderWise(a);
 
     }
+
+    public static void levelrderWise(Node root) {
+        Queue<Pair> q= new LinkedList<>();
+        q.add(new Pair(root,0));
+        int Currlevel=0;
+        while(q.size()>0){
+            Pair front = q.remove();
+            if(front.level != Currlevel){
+                Currlevel++;
+                System.out.println();
+            }
+            System.out.print(front.node.val+" ");
+            if(front.node.left != null) q.add(new Pair(front.node.left,front.level+1));
+            if(front.node.left != null) q.add(new Pair(front.node.right,front.level+1));
+        }
+
+    }
+
     public static void display(Node root){
         if(root == null)return;
         display(root.left);
@@ -63,4 +102,41 @@ public class Tree {
     public static int mini(Node root){
         return root ==null? Integer.MAX_VALUE :Math.min(root.val,Math.min(mini(root.left),mini(root.right)));
     }
+    public static int level(Node root){
+        return root == null ? 0: 1+Math.max(level(root.left),level(root.right));
+    }
+    public static void preorder(Node root){
+        if(root==null) return;
+        System.out.print(root.val);
+        preorder(root.left);
+        preorder(root.right);
+    }
+    public static void inorder(Node root){
+        if(root == null) return;
+
+        inorder(root.left);
+        System.out.print(root.val);
+        inorder(root.right);
+    }
+    public static void postorder(Node root){
+        if(root == null) return;
+
+        postorder(root.left);
+        System.out.print(root.val);
+        postorder(root.right);
+
+    }
+
+    public static void levelOrder(Node root){
+        Queue<Node> q = new LinkedList<Node>();
+        q.add(root);
+        while(q.size()>0){
+            Node front = q.remove();
+            System.out.print(front.val+" ");
+            if(front.left != null)q.add(front.left);
+            if(front.right != null)q.add(front.right);
+        }
+
+    }
+
 }
