e862a4a1ba830afa7221ed04791e15a3d20533db
Bibekananda Muni <bibekanandamuni@gmail.com>
2026-04-12 00:36:30 +0530

    Create CommonAncestor.java

---
+++ src/main/java/BinaryTree/CommonAncestor.java
@@ -0,0 +1,9 @@
+//235. Lowest Common Ancestor of a Binary Search Tree
+class Solution {
+    public TreeNode lowestCommonAncestor(TreeNode root, TreeNode p, TreeNode q) {
+
+      if(root.val > p.val && root.val >q.val) return lowestCommonAncestor(root.left,p,q);
+      else if(root.val <p.val && root.val <q.val) return lowestCommonAncestor(root.right,p,q);
+      else return root;  
+    }
+}
